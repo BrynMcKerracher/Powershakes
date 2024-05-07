ef73a8fda793a3560468b8d5c9429bcc545e0dc6
Bryn McKerracher <79728898+BrynMcKerracher@users.noreply.github.com>
2024-05-07 13:11:21 +1200

    Update to 1.1.0

--- Powershakes/Package/README.md
+++ Powershakes/Package/README.md
@@ -1,19 +1,14 @@
-﻿# Powershakes
-Adds the Guckshake to Valheim, which lets the player use Moder's sailing power.
+﻿# JotunnModStub
+Upon building for release, this README.md is copied into the `Package` folder for thunderstore packaging. Remember to also edit the manifest.json and supply your own mod icon.
 
 ## Installation (manual)
-1. Download and install BepInEx and Jotunn (these are required for this mod to work).
-2. Open your Valheim game directory.
-3. Open BepInEx/plugins.
-4. Paste Powershakes.dll into BepInEx/plugins.
-5. Done!
+
 
 ## Features
-Adds the Guckshake to Valheim, which can be crafted at any cauldron for 1 guck, 2 raspberries and 2 blueberries. 
-The shake lets the player use Moder's sailing power for 5 minutes while suffering audio-visual effects.
+
 
 ## Changelog
 
 
 ## Known issues
-You can find the github at: https://github.com/BrynMcKerracher/Powershakes
+You can find the github at:
--- Powershakes/Powershakes.cs
+++ Powershakes/Powershakes.cs
@@ -4,9 +4,11 @@ using Jotunn.Configs;
 using Jotunn.Entities;
 using Jotunn.Managers;
 using Jotunn.Utils;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
-using ValheimModdingWiki;
 
 namespace Powershakes
 {
@@ -18,7 +20,7 @@ namespace Powershakes
     {
         public const string PluginGUID = "TinyOak.Powershakes";
         public const string PluginName = "Powershakes";
-        public const string PluginVersion = "0.0.1";
+        public const string PluginVersion = "1.2.0";
         
         public static CustomLocalization Localization = LocalizationManager.Instance.GetLocalization();
         private readonly Harmony _harmonyPatcher = new(PluginGUID);
@@ -27,10 +29,14 @@ namespace Powershakes
         private static AssetBundle _powershakeAssets;
 
         //Custom Status Effects
-        private CustomStatusEffect _sailingPowerStatusEffect;
+        public static CustomStatusEffect SailingPowerStatusEffect;
+        public static CustomStatusEffect EikthyrPowerStatusEffect;
 
         //VFX/SFX
         public static GameObject GuckshakeSFXPrefab;
+
+        //RPCs
+        public static CustomRPC RpcGuckEffect;
 
         private void Awake()
         {
@@ -44,8 +50,31 @@ namespace Powershakes
             _harmonyPatcher.PatchAll();
             AddCustomStatusEffects();
 
+            RpcGuckEffect = NetworkManager.Instance.AddRPC("RpcGuckEffect", RpcGuckEffectServerReceive, RpcGuckEffectClientReceive);
+
             Jotunn.Logger.LogInfo("Loaded successfully!");
         }
+
+        #region RPCS
+        private IEnumerator RpcGuckEffectServerReceive(long sender, ZPackage package)
+        {
+            string playerName = package.ReadString();
+            Jotunn.Logger.LogInfo($"Server received from {playerName}");
+
+            ZPackage serverPackage = new();
+            serverPackage.Write(playerName);
+
+            RpcGuckEffect.SendPackage(ZNet.instance.m_peers, serverPackage);
+            PowershakeEffectsPatch.CreateGuckEffectsForPlayer(playerName);
+            yield return null;
+        }
+
+        private IEnumerator RpcGuckEffectClientReceive(long sender, ZPackage package)
+        {
+            PowershakeEffectsPatch.CreateGuckEffectsForPlayer(package.ReadString());
+            yield return null;
+        }
+        #endregion
 
         #region CUSTOM ITEMS
         /// <summary>
@@ -54,6 +83,9 @@ namespace Powershakes
         private void AddCustomItems()
         {
             AddGuckshakeItem();
+            AddBuckshakeItem();
+
+            PrefabManager.OnVanillaPrefabsAvailable -= AddCustomItems;
         }
 
         /// <summary>
@@ -70,7 +102,9 @@ namespace Powershakes
             guckShakeConfig.AddRequirement(new RequirementConfig("Raspberry", 2));
             guckShakeConfig.AddRequirement(new RequirementConfig("Blueberries", 2));
 
-            CustomItem guckShake = new CustomItem("Guckshake", "ShocklateSmoothie", guckShakeConfig);
+            CustomItem guckShake = new("Guckshake", "ShocklateSmoothie", guckShakeConfig);
+            guckShake.ItemDrop.m_itemData.m_shared.m_consumeStatusEffect = SailingPowerStatusEffect.StatusEffect;
+
             ItemManager.Instance.AddItem(guckShake);
 
             Localization.AddTranslation("English", new Dictionary<string, string>
@@ -78,10 +112,28 @@ namespace Powershakes
                 {"item_guckshake", "Guckshake"},
                 {"item_guckshake_desc", "Gotta go fast!"}
             });
+        }
 
-            PrefabManager.OnVanillaPrefabsAvailable -= AddCustomItems;
+        private void AddBuckshakeItem()
+        {
+            ItemConfig buckShakeConfig = new ItemConfig();
+            buckShakeConfig.Name = "$item_buckshake";
+            buckShakeConfig.Description = "$item_buckshake_desc";
+            buckShakeConfig.CraftingStation = CraftingStations.Cauldron;
+            buckShakeConfig.AddRequirement(new RequirementConfig("TrophyDeer", 1));
+            buckShakeConfig.AddRequirement(new RequirementConfig("Raspberry", 2));
+            buckShakeConfig.AddRequirement(new RequirementConfig("Blueberries", 2));
 
-            guckShake.ItemDrop.m_itemData.m_shared.m_consumeStatusEffect = _sailingPowerStatusEffect.StatusEffect;
+            CustomItem buckShake = new("Buckshake", "ShocklateSmoothie", buckShakeConfig);
+            buckShake.ItemDrop.m_itemData.m_shared.m_consumeStatusEffect = EikthyrPowerStatusEffect.StatusEffect;
+
+            ItemManager.Instance.AddItem(buckShake);
+
+            Localization.AddTranslation("English", new Dictionary<string, string>
+            {
+                {"item_buckshake", "Buckshake"},
+                {"item_buckshake_desc", "Move those trotters!"}
+            });
         }
         #endregion
 
@@ -92,6 +144,7 @@ namespace Powershakes
         private void AddCustomStatusEffects()
         {
             AddSailingPowerStatusEffect();
+            AddEikthyrPowerStatusEffect();
         }
 
         /// <summary>
@@ -120,33 +173,87 @@ namespace Powershakes
                 {"gutblaster_tooltip", "Your bodily gases will power the sails of your ship."}
             });
 
-            _sailingPowerStatusEffect = new CustomStatusEffect(effect, false);
+            SailingPowerStatusEffect = new CustomStatusEffect(effect, false);
 
-            ItemManager.Instance.AddStatusEffect(_sailingPowerStatusEffect);
+            ItemManager.Instance.AddStatusEffect(SailingPowerStatusEffect);
+        }
+
+        private void AddEikthyrPowerStatusEffect()
+        {
+            SE_Stats effect = ScriptableObject.CreateInstance<SE_Stats>();
+            effect.name = "Eikthyr Power";
+            effect.m_name = "$eikthyrpower_effectname";
+            effect.m_ttl = 300;
+            effect.m_tooltip = "$eikthyrpower_tooltip";
+            effect.m_startMessage = "$eikthyrpower_effectstart";
+            effect.m_stopMessage = "$eikthyrpower_effectstop";
+            effect.m_startMessageType = MessageHud.MessageType.Center;
+            effect.m_stopMessageType = MessageHud.MessageType.Center;
+            effect.m_runStaminaDrainModifier = -0.6f;
+            effect.m_jumpStaminaUseModifier = -0.6f;
+            effect.m_icon = _powershakeAssets.LoadAsset<Sprite>("assets/powershakes/eikthyr.png");
+
+            Localization.AddTranslation("English", new Dictionary<string, string>
+            {
+                {"eikthyrpower_effectname", "Eikthyr"},
+                {"eikthyrpower_effectstart", "The power of Eikthyr suffuses your veins"},
+                {"eikthyrpower_effectstop", "You feel Eikthyr's power fading"},
+                {"eikthyrpower_tooltip", "Running and jumping use 60% less stamina."}
+            });
+
+            EikthyrPowerStatusEffect = new CustomStatusEffect(effect, false);
+
+            ItemManager.Instance.AddStatusEffect(EikthyrPowerStatusEffect);
         }
         #endregion
 
         #region PATCHES STATUS EFFECTS
+        /// <summary>
+        /// Whenever the player eats, we first check if it's one of the powershakes and then propagate any 
+        /// effects to the server.
+        /// </summary>
         [HarmonyPatch(typeof(Player), nameof(Player.EatFood))]
         class PowershakeEffectsPatch
         {
             static void Prefix(Player __instance, ItemDrop.ItemData item)
             {
-                if (item.m_shared.m_consumeStatusEffect.m_name == "$gutblaster_effectname" && __instance.CanEat(item, false))
+                StatusEffect foodStatusEffect = item?.m_shared?.m_consumeStatusEffect;
+
+                if (foodStatusEffect == null || !__instance.CanEat(item, false))
                 {
-                    GameObject randomSFXPlayer = new();
-                    randomSFXPlayer.transform.parent = __instance.gameObject.transform;
-
-                    TimedDestruction timer = randomSFXPlayer.AddComponent<TimedDestruction>();
-                    timer.m_timeout = item.m_shared.m_consumeStatusEffect.m_ttl;
-                    timer.Trigger();
-
-                    RandomSFXPlayer sfx = randomSFXPlayer.AddComponent<RandomSFXPlayer>();
-                    sfx.Trigger(__instance.transform);
-
-                    GameObject gutBlasterEffectsVFX = Instantiate(_powershakeAssets.LoadAsset<GameObject>("assets/powershakes/vfx_guckshake_gaseffects.prefab"), __instance.gameObject.transform);
-                    gutBlasterEffectsVFX.transform.localPosition = new Vector3(0, 0.9f, 0);
+                    return;
                 }
+
+                if (foodStatusEffect.m_name == "$gutblaster_effectname")
+                {
+                    ZPackage package = new ZPackage();
+                    package.Write(__instance.GetPlayerName());
+
+                    RpcGuckEffect.SendPackage(ZNet.GetUID(), package);
+                }
+            }
+
+            public static void CreateGuckEffectsForPlayer(string playerName)
+            {
+                Player targetPlayer = FindObjectsOfType<Player>().First(n => n.GetPlayerName() == playerName);
+                if (targetPlayer == null)
+                {
+                    Jotunn.Logger.LogWarning($"Failed to create effects on player: '{playerName}' not found.");
+                    return;
+                }
+
+                GameObject randomSFXPlayer = new();
+                randomSFXPlayer.transform.parent = targetPlayer.gameObject.transform;
+
+                TimedDestruction timer = randomSFXPlayer.AddComponent<TimedDestruction>();
+                timer.m_timeout = PowershakesPlugin.SailingPowerStatusEffect.StatusEffect.m_ttl;
+                timer.Trigger();
+
+                RandomSFXPlayer sfx = randomSFXPlayer.AddComponent<RandomSFXPlayer>();
+                sfx.Trigger(targetPlayer.transform);
+
+                GameObject gutBlasterEffectsVFX = Instantiate(_powershakeAssets.LoadAsset<GameObject>("assets/powershakes/vfx_guckshake_gaseffects.prefab"), targetPlayer.gameObject.transform);
+                gutBlasterEffectsVFX.transform.localPosition = new Vector3(0, 0.9f, 0);
             }
         }
         #endregion 
